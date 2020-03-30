3ee949355c3977fdb7dbf92bff2a4a266c016128
WeiZhe Chang <weizhe.chang@gmail.com>
2020-03-30 23:48:02 +0800

    add remind

--- Model/DataStruc.go
+++ Model/DataStruc.go
@@ -3,13 +3,15 @@ package Model
 import "time"
 
 type User struct {
-	NTAccount  string    `bson:"ntaccount"`
-	RemindTime string    `bson:"remindtime"`
-	LineId     string    `bson:"lineid"`
-	ClaimTime  time.Time `bson:"claimtime`
+	NTAccount      string    `bson:"ntaccount"`
+	RemindTime     string    `bson:"remindtime"`
+	LineId         string    `bson:"lineid"`
+	ClaimTime      time.Time `bson:"claimtime`
+	LastRemindTime string    `bson:"lastremindtime"`
 }
 
 type User2 struct {
-	LineId     string    `bson:"lineid"`
-	RemindTime string    `bson:"remindtime"`
+	LineId         string `bson:"lineid"`
+	RemindTime     string `bson:"remindtime"`
+	LastRemindTime string `bson:"lastremindtime"`
 }
--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"github.com/line/line-bot-sdk-go/linebot"
 	"github.com/weizhe0422/GGQuestionnaireReminder/DBUtil"
 	"github.com/weizhe0422/GGQuestionnaireReminder/Model"
+	"go.mongodb.org/mongo-driver/bson"
 	"log"
 	"net/http"
 	"os"
@@ -17,9 +18,10 @@ var bot *linebot.Client
 var groupID string
 
 const (
-	mongoAtlas = "mongodb+srv://gguser:true0422@cluster0-lpy0f.gcp.mongodb.net/test?retryWrites=true&w=majority"
+	mongoAtlas    = "mongodb+srv://gguser:true0422@cluster0-lpy0f.gcp.mongodb.net/test?retryWrites=true&w=majority"
 	surveycakeURL = "https://zh.surveymonkey.com/r/EmployeeHealthCheck?fbclid=IwAR2fKoFAYPEHxwhNpxIcgFXzWXylYGcVfVRuNPS88VpKwwKi_40cavQZYFU"
 )
+
 func main() {
 	var err error
 
@@ -27,12 +29,12 @@ func main() {
 	log.Println("Bot:", bot, " err:", err)
 	http.HandleFunc("/callback", callbackHandler)
 
-	go func(){
-		for{
+	go func() {
+		for {
 			log.Println("開始提醒")
 			PushAlarmMessage()
 			log.Println("結束提醒")
-			time.Sleep(3*time.Minute)
+			time.Sleep(3 * time.Minute)
 		}
 	}()
 
@@ -41,10 +43,10 @@ func main() {
 	http.ListenAndServe(addr, nil)
 }
 
-func PushAlarmMessage(){
+func PushAlarmMessage() {
 	mongo := &DBUtil.MongoDB{
-		URL: mongoAtlas,
-		Database: "GGUser",
+		URL:        mongoAtlas,
+		Database:   "GGUser",
 		Collection: "QuestionReminder",
 	}
 
@@ -59,24 +61,36 @@ func PushAlarmMessage(){
 		log.Println("開始檢查")
 		var user Model.User2
 		err := allRecord.Decode(&user)
-		log.Println("ID:",user.LineId,"設定時間:",user.RemindTime)
+		log.Println("ID:", user.LineId, "設定時間:", user.RemindTime)
 		if err != nil {
 			log.Printf("failed to decode: %v", err)
 			continue
 		}
-		remindtime,_:=time.ParseInLocation("2006-01-02 15:04",time.Now().Format("2006-01-02")+ " "+user.RemindTime,timeLoc)
-		log.Println("提醒時間:",remindtime)
-		log.Println("現在時間(上海):",time.Now().In(timeLoc))
-		if remindtime.Before(time.Now().In(timeLoc)){
+		remindtime, _ := time.ParseInLocation("2006-01-02 15:04", time.Now().Format("2006-01-02")+" "+user.RemindTime, timeLoc)
+		log.Println("提醒時間:", remindtime)
+		log.Println("現在時間(上海):", time.Now().In(timeLoc))
+
+		lastRemindTime, _ := time.ParseInLocation("2005/01/02", user.LastRemindTime, timeLoc)
+		_, lMonth, lDay := lastRemindTime.Date()
+		_, rMonth, rDay := remindtime.Date()
+		log.Printf("上次提醒時間:%v", lastRemindTime)
+
+		if remindtime.Before(time.Now().In(timeLoc)) &&
+			((lMonth == rMonth && lDay < rDay) || (lMonth != rMonth)) {
 			log.Println("開始發送提醒")
 			_, err := bot.PushMessage(user.LineId, linebot.NewTextMessage("記得去填問卷啊！"+surveycakeURL)).Do()
 			if err != nil {
-				log.Printf("推送提提醒給%s失敗:%v",user.LineId,err)
+				log.Printf("推送提提醒給%s失敗:%v", user.LineId, err)
 				continue
 			}
-			log.Printf("推送提提醒給%s成功",user.LineId)
-		}else{
-			log.Printf("%s尚未到提醒時間%s",user.LineId,user.RemindTime)
+
+			log.Printf("推送提提醒給%s成功", user.LineId)
+			_, err = mongo.UpdateRecord(bson.M{"lineid": user.LineId}, bson.M{"$set": bson.M{"lastremindtime": time.Now().Format("2005/01/02 03:04:05")}})
+			if err != nil {
+				log.Printf("更新提醒時間失敗:%v", err)
+			}
+		} else {
+			log.Printf("%s尚未到提醒時間%s", user.LineId, user.RemindTime)
 		}
 		log.Println("結束檢查")
 	}
@@ -94,13 +108,13 @@ func callbackHandler(w http.ResponseWriter, r *http.Request) {
 	}
 
 	mongo := &DBUtil.MongoDB{
-		URL: mongoAtlas,
-		Database: "GGUser",
+		URL:        mongoAtlas,
+		Database:   "GGUser",
 		Collection: "QuestionReminder",
 	}
 
 	for _, event := range events {
-		log.Println("EVENT TYPE:",event.Type)
+		log.Println("EVENT TYPE:", event.Type)
 		switch event.Type {
 		case linebot.EventTypeMessage:
 			switch message := event.Message.(type) {
@@ -168,15 +182,15 @@ func callbackHandler(w http.ResponseWriter, r *http.Request) {
 						})).Do()
 			}
 		case linebot.EventTypePostback:
-			switch event.Postback.Data{
+			switch event.Postback.Data {
 			case "remindTime":
 				registInfo := Model.User{LineId: event.Source.UserID, RemindTime: event.Postback.Params.Time}
 				record, err := mongo.InsertOneRecord(registInfo)
 				if err != nil {
-					bot.PushMessage(event.Source.UserID,linebot.NewTextMessage("設定時間失敗，請重新嘗試!"))
+					bot.PushMessage(event.Source.UserID, linebot.NewTextMessage("設定時間失敗，請重新嘗試!"))
 				}
 				bot.PushMessage(event.Source.UserID, linebot.NewTextMessage("預定了每天"+event.Postback.Params.Time+"提醒填寫問卷!")).Do()
-				log.Println("Mongo insert info: ",record)
+				log.Println("Mongo insert info: ", record)
 			}
 		default:
 			bot.PushMessage(event.Source.UserID, linebot.NewTextMessage("DEFAULT")).Do()
